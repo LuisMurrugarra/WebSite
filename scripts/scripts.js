690b599776810d98bb33af14767c3ef5fd153cbc
Luis Murrugarra <luismurrugarra17@gmail.com>
2020-05-22 18:39:40 -0500

    Se agrego evento a submenu

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,7 +1,14 @@
 let buttonMenu = document.getElementById(`header__button`);
 let menu = document.getElementById(`menu`);
+let linkSubMenu = document.getElementById(`linkSubMenu`);
+let subMenu = document.getElementById(`subMenu`);
 if(menu && buttonMenu){
     buttonMenu.addEventListener(`click`,() =>{
         menu.classList.toggle(`menu--activate`);
     })
+}
+if(linkSubMenu && subMenu){
+    linkSubMenu.addEventListener(`click`,() =>{
+        subMenu.classList.toggle(`submenu--activate`);
+    });
 }
